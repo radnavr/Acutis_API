ff5e8ddb19735369943da565b4ffdf6d22e23e63
RadekN <radnavr@gmail.com>
2024-05-02 17:10:41 +0200

    TS1 shortcircuiting env vars

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv");
 const { parsed: config } = dotenv.config();
 
-const url = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image/tags/acutis`;
+const url = `https://api.cloudinary.com/v1_1/${
+  config.CLOUD_NAME || process.env.CLOUD_NAME
+}/resources/image/tags/acutis`;
 const auth = {
-  username: config.API_KEY,
-  password: config.API_SECRET,
+  username: config.API_KEY || process.env.API_KEY,
+  password: config.API_SECRET || process.env.API_SECRET,
 };
 
 module.exports = { url, auth };
